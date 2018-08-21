5e38a35e69b0e6ce0de97964cab602d5b927374b
nobu06 <nobu.rs06@gmail.com>
2018-08-21 15:36:32 +0900

    Initial commit - from the Unity tutorial

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour 
+{
+	private RoundData[] allRoundData;
+
+    private PlayerProgress playerProgress;
+
+    private string gameDataFileName = "data.json";
+
+	void Start ()  
+	{
+		DontDestroyOnLoad (gameObject);
+        LoadGameData();
+        LoadPlayerProgress();
+
+		SceneManager.LoadScene ("MenuScreen");
+	}
+	
+	public RoundData GetCurrentRoundData()
+	{
+		return allRoundData [0];
+	}
+
+    // submit a new score from the GameController
+    public void SubmitNewPlayerScore(int newScore)
+    {
+        if (newScore > playerProgress.highestScore)
+        {
+            playerProgress.highestScore = newScore;
+            SavePlayerProgress();
+        }
+    }
+
+    public int GetHighestPlayerScore()
+    {
+        return playerProgress.highestScore;
+    }
+
+    private void LoadPlayerProgress()
+    {
+        playerProgress = new PlayerProgress();
+
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            playerProgress.highestScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
+
+    private void SavePlayerProgress()
+    {
+        PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            // read all the text into a string
+            string dataAsJson = File.ReadAllText(filePath);
+
+            // deserialize it into a game data obj
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Editor/GameDataEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class GameDataEditor : EditorWindow
+{
+    public GameData gameData;
+
+    private string gameDataProjectFilePath = "/StreamingAssets/data.json";
+
+    [MenuItem ("Window/Game Data Editor")]
+    static void Init()
+    {
+        GameDataEditor window = (GameDataEditor)EditorWindow.GetWindow(typeof(GameDataEditor));
+        window.Show();
+    }
+
+	void OnGUI() 
+	{
+		// check if the gameData null
+        if (gameData != null)
+        {
+            // display and edit game obj
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("gameData");
+
+            EditorGUILayout.PropertyField(serializedProperty, true);
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (GUILayout.Button("Save data"))
+            {
+                SaveGameData();
+            }
+        }
+        if (GUILayout.Button("Load data"))
+        {
+            LoadGameData();
+        }
+
+	}
+
+	// load game data
+	private void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            gameData = JsonUtility.FromJson<GameData>(dataAsJson);
+        }
+        // possible that the data file does not exist so,
+        else 
+        {
+            gameData = new GameData();
+        }
+    }
+
+    // save game data
+    private void SaveGameData()
+    {
+        string dataAsJson = JsonUtility.ToJson(gameData);
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        File.WriteAllText(filePath, dataAsJson);
+    }
+}
