bdd8c1a7a5f847f0b3edc0702d6b26cbe97f32e0
nobu06 <nobu.rs06@gmail.com>
2018-08-22 11:59:04 +0900

    edit the UI panels to extract localized text at run-time; upgrade the GameData Editor window functionality

--- Assets/Scripts/Editor/GameDataEditor.cs
+++ Assets/Scripts/Editor/GameDataEditor.cs
@@ -8,18 +8,18 @@ public class GameDataEditor : EditorWindow
 {
     public GameData gameData;
 
-    private string gameDataProjectFilePath = "/StreamingAssets/data.json";
+    //private string gameDataProjectFilePath = "/StreamingAssets/data.json";
 
-    [MenuItem ("Window/Game Data Editor")]
+    [MenuItem("Window/Game Data Editor")]
     static void Init()
     {
         GameDataEditor window = (GameDataEditor)EditorWindow.GetWindow(typeof(GameDataEditor));
         window.Show();
     }
 
-	void OnGUI() 
-	{
-		// check if the gameData null
+    void OnGUI()
+    {
+        // check if the gameData null
         if (gameData != null)
         {
             // display and edit game obj
@@ -40,30 +40,122 @@ public class GameDataEditor : EditorWindow
             LoadGameData();
         }
 
-	}
+        if (GUILayout.Button("Create data"))
+        {
+            CreateNewData();
+        }
 
-	// load game data
-	private void LoadGameData()
+    }
+
+    // load game data
+    private void LoadGameData()
     {
-        string filePath = Application.dataPath + gameDataProjectFilePath;
-
-        if (File.Exists(filePath))
+        string filePath = EditorUtility.OpenFilePanel("Select game data file",
+                                                      Application.streamingAssetsPath,
+                                                      "json");
+        if (!string.IsNullOrEmpty(filePath))
         {
             string dataAsJson = File.ReadAllText(filePath);
-            gameData = JsonUtility.FromJson<GameData>(dataAsJson);
+            gameData = JsonUtility.FromJson<GameData>(dataAsJson);   
         }
-        // possible that the data file does not exist so,
-        else 
-        {
-            gameData = new GameData();
-        }
+
+        //// possible that the data file does not exist so,
+        //else
+        //{
+        //    gameData = new GameData();
+        //}
     }
 
     // save game data
     private void SaveGameData()
     {
-        string dataAsJson = JsonUtility.ToJson(gameData);
-        string filePath = Application.dataPath + gameDataProjectFilePath;
-        File.WriteAllText(filePath, dataAsJson);
+        string filePath = EditorUtility.SaveFilePanel("Save localization data file",
+                                                      Application.streamingAssetsPath,
+                                                     "",
+                                                      "json");
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            string dataAsJson = JsonUtility.ToJson(gameData);
+
+            File.WriteAllText(filePath, dataAsJson);
+        }
     }
+
+
+    private void CreateNewData()
+    {
+        gameData = new GameData();
+    }
+
 }
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//using System.IO;
+
+//public class GameDataEditor : EditorWindow
+//{
+//    public GameData gameData;
+
+//    private string gameDataProjectFilePath = "/StreamingAssets/data.json";
+
+//    [MenuItem ("Window/Game Data Editor")]
+//    static void Init()
+//    {
+//        GameDataEditor window = (GameDataEditor)EditorWindow.GetWindow(typeof(GameDataEditor));
+//        window.Show();
+//    }
+
+//	void OnGUI() 
+//	{
+//		// check if the gameData null
+//        if (gameData != null)
+//        {
+//            // display and edit game obj
+//            SerializedObject serializedObject = new SerializedObject(this);
+//            SerializedProperty serializedProperty = serializedObject.FindProperty("gameData");
+
+//            EditorGUILayout.PropertyField(serializedProperty, true);
+
+//            serializedObject.ApplyModifiedProperties();
+
+//            if (GUILayout.Button("Save data"))
+//            {
+//                SaveGameData();
+//            }
+//        }
+//        if (GUILayout.Button("Load data"))
+//        {
+//            LoadGameData();
+//        }
+
+//	}
+
+//	// load game data
+//	private void LoadGameData()
+//    {
+//        string filePath = Application.dataPath + gameDataProjectFilePath;
+
+//        if (File.Exists(filePath))
+//        {
+//            string dataAsJson = File.ReadAllText(filePath);
+//            gameData = JsonUtility.FromJson<GameData>(dataAsJson);
+//        }
+//        // possible that the data file does not exist so,
+//        else 
+//        {
+//            gameData = new GameData();
+//        }
+//    }
+
+//    // save game data
+//    private void SaveGameData()
+//    {
+//        string dataAsJson = JsonUtility.ToJson(gameData);
+//        string filePath = Application.dataPath + gameDataProjectFilePath;
+//        File.WriteAllText(filePath, dataAsJson);
+//    }
+//}
