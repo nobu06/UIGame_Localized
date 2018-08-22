07be77dba82c2625b2786cd5eda8914a6d7df58a
nobu06 <nobu.rs06@gmail.com>
2018-08-22 10:53:22 +0900

    load localizedUI and localizedGameData separately into the game

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -3,27 +3,43 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-public class DataController : MonoBehaviour 
+public class DataController : MonoBehaviour
 {
-	private RoundData[] allRoundData;
+    public static DataController instance;
+
+    private RoundData[] allRoundData;
 
     private PlayerProgress playerProgress;
 
-    private string gameDataFileName = "data.json";
+    private bool isReady = false;
 
-	void Start ()  
-	{
-		DontDestroyOnLoad (gameObject);
-        LoadGameData();
-        LoadPlayerProgress();
+    /** 
+     * turning this obj into a singleton
+     */ 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
 
-		SceneManager.LoadScene ("MenuScreen");
-	}
-	
-	public RoundData GetCurrentRoundData()
-	{
-		return allRoundData [0];
-	}
+        DontDestroyOnLoad(gameObject);
+    }
+    
+
+    void Start()
+    {
+        LoadPlayerProgress();       // load highest score
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
 
     // submit a new score from the GameController
     public void SubmitNewPlayerScore(int newScore)
@@ -55,9 +71,12 @@ public class DataController : MonoBehaviour
         PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
     }
 
-    private void LoadGameData()
+    /**
+     * Loads the game data in the language selected
+     */
+    public void LoadGameData(string fileName)
     {
-        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
 
         if (File.Exists(filePath))
         {
@@ -73,7 +92,123 @@ public class DataController : MonoBehaviour
         {
             Debug.LogError("Cannot load game data!");
         }
+
+        isReady = true;
+
     }
 
+    public bool GetIsReady()
+    {
+        return isReady;
+    }
 
 }
+
+
+
+//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.SceneManagement;
+//using System.IO;
+
+//public class DataController : MonoBehaviour 
+//{
+//	private RoundData[] allRoundData;
+
+//    private PlayerProgress playerProgress;
+
+//    //private string gameDataFileName = "data.json";
+
+
+
+//	void Start ()  
+//	{
+//		DontDestroyOnLoad (gameObject);
+//        LoadGameData();
+//        LoadPlayerProgress();
+
+//		SceneManager.LoadScene ("MenuScreen");
+//	}
+	
+//	public RoundData GetCurrentRoundData()
+//	{
+//		return allRoundData [0];
+//	}
+
+//    // submit a new score from the GameController
+//    public void SubmitNewPlayerScore(int newScore)
+//    {
+//        if (newScore > playerProgress.highestScore)
+//        {
+//            playerProgress.highestScore = newScore;
+//            SavePlayerProgress();
+//        }
+//    }
+
+//    public int GetHighestPlayerScore()
+//    {
+//        return playerProgress.highestScore;
+//    }
+
+//    private void LoadPlayerProgress()
+//    {
+//        playerProgress = new PlayerProgress();
+
+//        if (PlayerPrefs.HasKey("highestScore"))
+//        {
+//            playerProgress.highestScore = PlayerPrefs.GetInt("highestScore");
+//        }
+//    }
+
+//    private void SavePlayerProgress()
+//    {
+//        PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
+//    }
+
+//    /**
+//     * Loads the game data in the language selected
+//     */ 
+//    private void LoadGameData(string fileName)
+//    {
+
+//        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+//        if (File.Exists(filePath))
+//        {
+//            // read all the text into a string
+//            string dataAsJson = File.ReadAllText(filePath);
+
+//            // deserialize it into a game data obj
+//            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+//            allRoundData = loadedData.allRoundData;
+//        }
+//        else
+//        {
+//            Debug.LogError("Cannot load game data!");
+//        }
+
+//    }
+
+//    //private void LoadGameData()
+//    //{
+//    //    string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+//    //    if (File.Exists(filePath))
+//    //    {
+//    //        // read all the text into a string
+//    //        string dataAsJson = File.ReadAllText(filePath);
+
+//    //        // deserialize it into a game data obj
+//    //        GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+//    //        allRoundData = loadedData.allRoundData;
+//    //    }
+//    //    else
+//    //    {
+//    //        Debug.LogError("Cannot load game data!");
+//    //    }
+//    //}
+
+
+//}
--- Assets/Scripts/Localization/StartupManager.cs
+++ Assets/Scripts/Localization/StartupManager.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ * Loads the next scene when DataController and LocalizationManager
+ * scripts have finished loading the localized texts.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +12,7 @@ public class StartupManager : MonoBehaviour {
 
 	private IEnumerator Start()
 	{
-        while(!LocalizationManager.instance.GetIsReady())
+        while(!LocalizationManager.instance.GetIsReady() || !DataController.instance.GetIsReady())
         {
             yield return null;
         }
